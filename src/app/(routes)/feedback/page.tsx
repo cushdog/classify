7ca90a224bf900867245e56b1de514e925a234c7
tylerjc5 <tylerjc5@illinois.edu>
2024-10-01 01:43:49 -0500

    Removed type errors for feedback page

--- src/app/(routes)/feedback/page.tsx
+++ src/app/(routes)/feedback/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import {
