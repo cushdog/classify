ef340cbfb70a6bf755e8b793226b11aa8690c3e1
tylerjc5 <tylerjc5@illinois.edu>
2024-10-01 01:41:42 -0500

    Added feedback in

---
+++ src/app/(routes)/feedback/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+const GoogleFormPage: React.FC = () => {
+
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    feedbackType: "",
+    feedback: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSelectChange = (value: string) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      feedbackType: value,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const googleFormURL =
+      "https://docs.google.com/forms/d/e/1FAIpQLSczIeiz7eJcUZLWfW-N0hsDw5mCaS8KlHVnrUH0ogL0sQFg1g/formResponse";
+
+    const submissionData = new FormData();
+    submissionData.append("entry.966562401", formData.feedbackType); // Feedback Type field
+    submissionData.append("entry.1715496086", formData.feedback); // Feedback text
+
+    try {
+      await fetch(googleFormURL, {
+        method: "POST",
+        body: submissionData,
+        mode: "no-cors",
+      });
+      toast.success("Your feedback has been submitted!");
+    } catch (error) {
+      console.error("Error submitting form: ", error);
+      toast.error("An error occurred while submitting your feedback. Please try again later.");
+    }
+  };
+
+  const handleBack = () => {
+    router.back();
+  }
+
+  return (
+    <div>
+      <Button onClick={handleBack}>Back</Button>
+      <h1>Submit Feedback</h1>
+      <form onSubmit={handleSubmit}>
+
+        <div>
+          <label htmlFor="feedbackType">Feedback Type*</label>
+          <Select onValueChange={handleSelectChange} required>
+            <SelectTrigger aria-label="Select Feedback Type">
+              <SelectValue placeholder="Select Feedback Type" />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectItem value="Comments">Comments</SelectItem>
+              <SelectItem value="Suggestions">Suggestions</SelectItem>
+              <SelectItem value="Questions">Questions</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div>
+          <label htmlFor="feedback">Feedback*</label>
+          <Textarea
+            name="feedback"
+            id="feedback"
+            value={formData.feedback}
+            onChange={handleChange}
+            required
+          ></Textarea>
+        </div>
+
+        <div>
+          <Button type="submit">Submit</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default GoogleFormPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,10 @@ export default function SearchPage() {
   const [search, setSearch] = useState<string>("");
   const router = useRouter();
 
+  const handleFeedbackClick = () => {
+    router.push("/feedback");
+  };
+
   const handleInputChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
       setSearch(e.target.value);
@@ -26,13 +30,14 @@ export default function SearchPage() {
   );
 
   const searchWithoutTerm = (searchQuery: string) => {
-    
     const mostRecentTerm = `${semesterConfigs[0].semester} ${semesterConfigs[0].year}`;
     const isSubjectSearch = !/\d{3}/.test(searchQuery);
     const formattedQuery = isSubjectSearch
       ? searchQuery
       : searchQuery.replace(/([a-zA-Z])(\d)/, "$1 $2");
-    const modified_search = `${formattedQuery} ${semesterConfigs[0].semester.toLowerCase()} ${semesterConfigs[0].year}`;
+    const modified_search = `${formattedQuery} ${semesterConfigs[0].semester.toLowerCase()} ${
+      semesterConfigs[0].year
+    }`;
 
     const url = `https://uiuc-course-api-production.up.railway.app/search?query=${encodeURIComponent(
       modified_search
@@ -41,33 +46,44 @@ export default function SearchPage() {
 
     try {
       if (isSubjectSearch) {
-        router.push(`/subject?subject=${searchQuery}&term=${encodeURIComponent(
-          mostRecentTerm
-        )}`);
+        router.push(
+          `/subject?subject=${searchQuery}&term=${encodeURIComponent(
+            mostRecentTerm
+          )}`
+        );
       } else {
-        router.push(`/class?class=${formattedQuery}&term=${encodeURIComponent(
-          mostRecentTerm
-        )}`);
+        router.push(
+          `/class?class=${formattedQuery}&term=${encodeURIComponent(
+            mostRecentTerm
+          )}`
+        );
       }
     } catch (error) {
       console.error("Error fetching data:", error);
       toast.error("Error fetching data");
     }
-  }
+  };
 
   return (
     <div>
       <div className="min-h-screen flex items-center justify-center">
         <div className="w-full max-w-2xl p-4 flex flex-col space-y-4">
           <div className="flex items-center space-x-2">
-            <Input value={search} onChange={handleInputChange} onSubmit={() => searchWithoutTerm(search)} />
+            <Input
+              value={search}
+              onChange={handleInputChange}
+              onSubmit={() => searchWithoutTerm(search)}
+            />
           </div>
           <Button
-                onClick={() => searchWithoutTerm(search)}
-                className="bg-black text-white p-2 rounded-lg hover:bg-black/80 w-full"
-              >
-                Search
-              </Button>
+            onClick={() => searchWithoutTerm(search)}
+            className="bg-black text-white p-2 rounded-lg hover:bg-black/80 w-full"
+          >
+            Search
+          </Button>
+          <Button
+            onClick={handleFeedbackClick}
+            className="bg-black text-white p-2 rounded-lg hover:bg-black/80 w-full">Feedback</Button>
         </div>
       </div>
     </div>
