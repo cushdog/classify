242ac6ac0d032357d9bdfed2ab48a42add1fe256
tylerjc5 <tylerjc5@illinois.edu>
2024-10-01 19:48:31 -0500

    Made the subject grid responsive

--- src/app/(routes)/subject/page.tsx
+++ src/app/(routes)/subject/page.tsx
@@ -6,20 +6,21 @@ import { Suspense } from "react";
 import { fetchData, Course } from "@/lib/commonFunctions";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
-import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import { semesterConfigs } from "@/lib/commonFunctions";
 
 const SubjectDetails: React.FC = () => {
-  
   const [subjectData, setSubjectData] = useState<Course[][] | null>(null);
 
   const router = useRouter();
   const searchParams = useSearchParams();
 
-
-
   useEffect(() => {
-
     const setupClassData = async () => {
       const subjectParam = searchParams.get("subject")?.split(" ");
       const termParam = searchParams.get("term")?.split(" ");
@@ -41,13 +42,11 @@ const SubjectDetails: React.FC = () => {
     };
 
     const fetchDataAndUpdateState = async () => {
-
       const data = await setupClassData();
 
-      const uniqueData = data.filter((item: Course[], index: number, self: Course[][]) =>
-        index === self.findIndex((t) => (
-          t[2] === item[2] && t[3] === item[3]
-        ))
+      const uniqueData = data.filter(
+        (item: Course[], index: number, self: Course[][]) =>
+          index === self.findIndex((t) => t[2] === item[2] && t[3] === item[3])
       );
 
       setSubjectData(uniqueData);
@@ -56,51 +55,57 @@ const SubjectDetails: React.FC = () => {
     fetchDataAndUpdateState();
   }, []);
 
+  const handleClassClick = (classNumber: string) => {
+    const mostRecentTerm = `${semesterConfigs[0].semester} ${semesterConfigs[0].year}`;
 
-const handleClassClick = (classNumber: string) => {
+    router.push(
+      `/class?class=${classNumber}&term=${encodeURIComponent(mostRecentTerm)}`
+    );
+  };
 
-  const mostRecentTerm = `${semesterConfigs[0].semester} ${semesterConfigs[0].year}`;
-
-  router.push(
-    `/class?class=${classNumber}&term=${encodeURIComponent(
-      mostRecentTerm
-    )}`
-  );
-
-}
-
-const handleBack = () => {
-  router.back();
-}
-  
+  const handleBack = () => {
+    router.back();
+  };
 
   return (
     <div>
-    <Button onClick={handleBack}>Back</Button>
-    <div className="grid grid-cols-3 gap-4">
-    {subjectData && subjectData.map((classData, index) => (
-      <Card key={index} className="max-w-md mx-auto mt-8 overflow-hidden bg-gradient-to-br from-blue-500 to-purple-600 text-white shadow-lg">
-        <CardHeader className="bg-black bg-opacity-30 p-4">
-          <h2 className="text-2xl font-bold">{`${classData[2]} ${classData[3]}`}</h2>
-          <p className="text-lg">{String(classData[4])}</p>
-        </CardHeader>
-        <CardContent className="p-4 space-y-2">
-          <p><strong>Description:</strong> {String(classData[5])}</p>
-          <p><strong>Credit Hours:</strong> {String(classData[6])}</p>
-          <p><strong>Average GPA:</strong> {String(classData[22])}</p>
-        </CardContent>
-        <CardFooter className="bg-black bg-opacity-30 p-4">
-          <Button 
-            onClick={() => handleClassClick(`${classData[2]} ${classData[3]}`)} 
-            variant="secondary"
-            className="bg-white text-blue-600 hover:bg-blue-100 hover:text-blue-800 transition-colors"
-          >
-            <ArrowLeft className="mr-2 h-4 w-4" /> Visit Class
-          </Button>
-        </CardFooter>
-      </Card>
-    ))}
-  </div>
+      <Button onClick={handleBack}>Back</Button>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {subjectData &&
+          subjectData.map((classData, index) => (
+            <Card
+              key={index}
+              className="max-w-md mx-auto mt-8 overflow-hidden bg-gradient-to-br from-blue-500 to-purple-600 text-white shadow-lg"
+            >
+              <CardHeader className="bg-black bg-opacity-30 p-4">
+                <h2 className="text-2xl font-bold">{`${classData[2]} ${classData[3]}`}</h2>
+                <p className="text-lg">{String(classData[4])}</p>
+              </CardHeader>
+              <CardContent className="p-4 space-y-2">
+                <p>
+                  <strong>Description:</strong> {String(classData[5])}
+                </p>
+                <p>
+                  <strong>Credit Hours:</strong> {String(classData[6])}
+                </p>
+                <p>
+                  <strong>Average GPA:</strong> {String(classData[22])}
+                </p>
+              </CardContent>
+              <CardFooter className="bg-black bg-opacity-30 p-4">
+                <Button
+                  onClick={() =>
+                    handleClassClick(`${classData[2]} ${classData[3]}`)
+                  }
+                  variant="secondary"
+                  className="bg-white text-blue-600 hover:bg-blue-100 hover:text-blue-800 transition-colors"
+                >
+                  <ArrowLeft className="mr-2 h-4 w-4" /> Visit Class
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+      </div>
     </div>
   );
 };
