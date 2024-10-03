b1fef5722472c967f8ea8c9f229ce7c066c60e3f
KatherineSick <katysick11@gmail.com>
2024-10-02 23:16:00 -0500

    Added a navbar at the top of each page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from 'react-hot-toast';
 import { Analytics } from "@vercel/analytics/react";
+import RealNavbar from "@/Custom Components/navbar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <RealNavbar></RealNavbar>
         <Toaster position="top-left" reverseOrder={false} />
             {children}
         <Analytics />
