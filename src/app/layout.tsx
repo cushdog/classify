4ebb91de685261031e7dedb8c152779674148356
tylerjc5 <tylerjc5@illinois.edu>
2024-10-03 00:22:22 -0500

    Added in speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from 'react-hot-toast';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,6 +24,7 @@ export default function RootLayout({
         <Toaster position="top-left" reverseOrder={false} />
             {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
