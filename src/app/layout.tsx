884a4518e85c60503af6d735791cd7746e98add0
tylerjc5 <tylerjc5@illinois.edu>
2024-10-03 01:11:34 -0500

    Took the navbar out for now since it's fairly buggy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import RealNavbar from "@/Custom Components/Nav/KatyNav/page";
 
 import "./globals.css";
 
@@ -23,7 +22,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Toaster position="top-left" reverseOrder={false} />
-        <RealNavbar />
         {children}
         <Analytics />
         <SpeedInsights />
