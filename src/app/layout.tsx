8a25c580ae07f733f942b9a7b6e3e1fcf598622a
tylerjc5 <tylerjc5@illinois.edu>
2024-10-12 00:33:13 -0500

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import RealNavbar from "@/Custom Components/ui/Nav/navbar";
 import Head from "next/head";
+import Script from "next/script";
 import "./globals.css";
 import ClientOnlyToastContainer from "@/lib/clientToast";
 
@@ -31,6 +32,19 @@ export default function RootLayout({
     <html lang="en" className={mulish.variable}>
       <Head>
         <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-CJX91GC6JV"
+        ></Script>
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-CJX91GC6JV');
+          `}
+        </Script>
       </Head>
       <body>
         <ThemeProvider theme={theme}>
