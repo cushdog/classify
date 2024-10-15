d3eb9cc637fb2772274761da73ce6ab83ba52a0c
Tyler Cushing <92754165+cushdog@users.noreply.github.com>
2024-10-15 15:07:02 -0500

    Error with use effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
