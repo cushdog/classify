e0fd6147f4f9388d6a304079bc25a76cca9ed3aa
tylerjc5 <tylerjc5@illinois.edu>
2024-11-25 18:59:05 -0600

    I think I fixed the type errors?

--- src/Custom Components/ui/Nav/navbar.tsx
+++ src/Custom Components/ui/Nav/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useTheme } from '@mui/material/styles';
+// import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import MobileComponent from "../Nav/Mobile Nav/navbarMobile";
 import DesktopComponent from "../Nav/Desktop Nav/navbarDesktop";
