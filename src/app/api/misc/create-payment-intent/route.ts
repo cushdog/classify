dce387a02e81d24d3950815a8b8dba1fde94e698
tylerjc5 <tylerjc5@illinois.edu>
2024-12-14 21:29:13 -0600

    Added in a bunch more changes

--- src/app/api/misc/create-payment-intent/route.ts
+++ src/app/api/misc/create-payment-intent/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-09-30.acacia",
+  apiVersion: "2024-11-20.acacia",
 });
 
 export async function POST(request: NextRequest) {
